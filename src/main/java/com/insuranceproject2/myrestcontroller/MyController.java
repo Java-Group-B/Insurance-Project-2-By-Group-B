44fad304ba33ec74d0a57bb863ed6162a2acee71
Irfan-123 <Irfan@123.gmail>
2024-03-09 13:22:43 +0530

    CR-795 development+testing completed

--- src/main/java/com/insuranceproject2/myrestcontroller/MyController.java
+++ src/main/java/com/insuranceproject2/myrestcontroller/MyController.java
@@ -29,19 +29,49 @@ public class MyController {
 	@Autowired
 	private ClaimService claimService;
 
-	@PostMapping("/saveUserClaim")
+	@PostMapping("/795saveUserClaim")
 	@Transactional
 	public User saveUser(@RequestBody User user) {
+		if (user.getFirstName() == null && user.getLastName() == null && user.getEmailId() == null) {
+			throw new IncompleteDetailsFilledException("Please Enter User Details");
+		}
+		if (user.getFirstName() == null || user.getFirstName() == "") {
+			throw new IncompleteDetailsFilledException("FirstName cannot be blank.Please enter Firstname");
+
+		}
+		if (user.getLastName() == null || user.getLastName() == "") {
+			throw new IncompleteDetailsFilledException("LastName cannot be blank.Please enter Lastname");
+		}
+		if (user.getEmailId() == null || user.getEmailId() == "") {
+			throw new IncompleteDetailsFilledException("Email id cannot blank.Please Enter valid mail id.");
+		}
 		User user1 = userService.saveUser(user);
 		List<Claim> claims = user.getClaimList();
 
 		if (claims.isEmpty()) {
-
 			throw new IncompleteDetailsFilledException(
 					"Claim should not be blank. Please enter at least one Claim request");
 		}
 
 		for (Claim claim : claims) {
+
+			if (claim.getClaimType() == null||claim.getClaimType() == "") {
+				throw new IncompleteDetailsFilledException(
+						"ClaimType should not be blank. Please Enter valid ClaimType");
+			}
+			if (claim.getClaimCause() == null||claim.getClaimCause() == "") {
+				throw new IncompleteDetailsFilledException(
+						"ClaimType should not be blank. Please Enter valid ClaimType");
+			}
+			if (claim.getClaimAmount() == null) {
+				throw new IncompleteDetailsFilledException(
+						"ClaimAmount should not be blank. Please Enter valid ClaimAmount");
+			}
+			if (claim.getClaimAmount() < 0) {
+				throw new IncompleteDetailsFilledException(
+						"ClaimAmount should not be Negative. Please Enter valid ClaimAmount");
+			}
+
 			claim.setUserId(user.getId());
 			claimService.saveClaim(claim);
 		}
--- src/main/java/com/insuranceproject2/service/impl/UserServiceImpl.java
+++ src/main/java/com/insuranceproject2/service/impl/UserServiceImpl.java
@@ -16,19 +16,7 @@ public class UserServiceImpl implements UserService {
 
 	@Override
 	public User saveUser(User user) {
-		if (user.getFirstName() == null && user.getLastName() == null && user.getEmailId() == null) {
-			throw new IncompleteDetailsFilledException("Please Enter User Details");
-		}
-		if (user.getFirstName() == null) {
-			throw new IncompleteDetailsFilledException("FirstName cannot be blank.Please enter Firstname");
-
-		}
-		if (user.getLastName() == null) {
-			throw new IncompleteDetailsFilledException("LastName cannot be blank.Please enter Lastname");
-		}
-		if (user.getEmailId() == null) {
-			throw new IncompleteDetailsFilledException("Email id cannot blank.Please Enter valid mail id.");
-		}
+		
 
 		User user1 = userRepository.save(user);
 		return user1;
